e58f0875470a7429a801c9f748d786946f5dc209
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2020-02-23 12:27:03 +0100

    setRgb
    Signed-off-by: Manuel Daniel Dahmen <manuel.dahmen@gmx.com>

--- src/main/java/pixelart/base/BaseMovieGenerator.java
+++ src/main/java/pixelart/base/BaseMovieGenerator.java
@@ -1,6 +1,9 @@
-/*     */ package pixelart.base;
+/*     */
+package pixelart.base;
 /*     */ 
-/*     */ import java.awt.Color;
+/*     */
+
+import java.awt.Color;
 /*     */ import java.awt.Dimension;
 /*     */ import java.awt.Graphics;
 /*     */ import java.awt.Graphics2D;
@@ -12,151 +15,282 @@
 /*     */ import javax.swing.JFrame;
 /*     */ import javax.swing.JLabel;
 /*     */ import javax.swing.JPanel;
-/*     */ 
+
+/*     */
 /*     */ public abstract class BaseMovieGenerator
 /*     */ {
-/*  18 */   private String dossier = "";
-/*  19 */   private String prefix = "";
-/*  20 */   protected int largeur = 1388;
-/*  21 */   protected int hauteur = 768;
-/*  22 */   private int frame = 100000;
-/*  23 */   protected BufferedImage image = null;
-/*  24 */   protected Graphics2D g = null;
-/*     */ 
+    /*  18 */   private String dossier = "";
+    /*  19 */   private String prefix = "";
+    /*  20 */   protected int largeur = 1388;
+    /*  21 */   protected int hauteur = 768;
+    /*  22 */   private int frame = 100000;
+    /*  23 */   protected BufferedImage image = null;
+    /*  24 */   protected Graphics2D g = null;
+    /*     */
 /*     */ 
 /*     */   
 /*     */   protected Graphics gPrim;
-/*     */ 
+    /*     */
 /*     */ 
 /*     */   
 /*     */   protected JPanel jPanel;
-/*     */ 
+    /*     */
 /*     */ 
 /*     */   
 /*     */   protected int frame0;
-/*     */ 
+    /*     */
 /*     */ 
 /*     */   
 /*     */   private JLabel label;
+    private Graphics g2;
+
+    /*     */
+/*     */ 
+/*     */   
+/*     */
+    public BaseMovieGenerator(int largeur, int hauteur) {
+/*  45 */
+        this();
+/*  46 */
+        this.largeur = largeur;
+/*  47 */
+        this.hauteur = hauteur;
+/*     */
+    }
+
+    /*     */
+/*     */ 
+/*     */   
+/*     */
+    public void creerImage() {
+/*  53 */
+        this.image = new BufferedImage(this.largeur, this.hauteur, BufferedImage.TYPE_INT_ARGB);
+/*  54 */
+        this.g = this.image.createGraphics();
+        this.g2 = this.jPanel.getGraphics();
+/*     */
+    }
+
+    /*     */
+/*     */ 
 /*     */ 
 /*     */ 
 /*     */   
-/*     */   public BaseMovieGenerator(int largeur, int hauteur) {
-/*  45 */     this();
-/*  46 */     this.largeur = largeur;
-/*  47 */     this.hauteur = hauteur;
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public void creerImage() {
-/*  53 */     this.image = new BufferedImage(this.largeur, this.hauteur, 1);
-/*  54 */     this.g = this.image.createGraphics();
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public void enregistrerImage() throws IOException {
-/*  62 */     String filename = String.valueOf(this.dossier) + this.prefix + File.pathSeparator + this.frame + 
+/*     */
+    public void enregistrerImage() throws IOException {
+/*  62 */
+        String filename = String.valueOf(this.dossier) + this.prefix + File.pathSeparator + this.frame +
 /*  63 */       ".jpg";
-/*  64 */     this.frame++;
-/*  65 */     ImageIO.write(this.image, "jpeg", new File(filename));
-/*  66 */     this.g.dispose();
-/*     */   }
-/*     */ 
+/*  64 */
+        this.frame++;
+/*  65 */
+        ImageIO.write(this.image, "jpg", new File(filename));
+/*  66 */
+/*     */
+    }
+
+    /*     */
 /*     */ 
 /*     */   
-/*     */   public void initMontrerImage() {
-/*  72 */     JFrame jjFrame = new JFrame("Video");
-/*  73 */     this.jPanel = new JPanel();
-/*  74 */     this.jPanel.setMinimumSize(new Dimension(this.largeur, this.hauteur));
-/*  75 */     jjFrame.add(this.jPanel);
-/*  76 */     jjFrame.setContentPane(this.jPanel);
-/*  77 */     jjFrame.setMinimumSize(new Dimension(this.largeur, this.hauteur));
-/*  78 */     this.jPanel.setBounds(0, 0, this.largeur, this.hauteur);
-/*  79 */     jjFrame.setDefaultCloseOperation(3);
-/*  80 */     jjFrame.pack();
-/*  81 */     jjFrame.setVisible(true);
+/*     */
+    public void initMontrerImage() {
+/*  72 */
+        JFrame jjFrame = new JFrame("Video");
+/*  73 */
+        this.jPanel = new JPanel();
+/*  74 */
+        this.jPanel.setMinimumSize(new Dimension(this.largeur, this.hauteur));
+/*  75 */
+        jjFrame.add(this.jPanel);
+/*  76 */
+        jjFrame.setContentPane(this.jPanel);
+/*  77 */
+        jjFrame.setMinimumSize(new Dimension(this.largeur, this.hauteur));
+/*  78 */
+        this.jPanel.setBounds(0, 0, this.largeur, this.hauteur);
+/*  79 */
+        jjFrame.setDefaultCloseOperation(3);
+/*  80 */
+        jjFrame.pack();
+/*  81 */
+        jjFrame.setVisible(true);
 /*     */     
-/*  83 */     this.gPrim = this.jPanel.getGraphics();
-/*  84 */     this.gPrim.setColor(Color.black);
-/*  85 */     this.gPrim.drawRect(0, 0, 100, 100);
-/*     */   }
-/*     */ 
+/*  83 */
+        //this.gPrim = this.jPanel.getGraphics();
+/*  84 */
+        //this.gPrim.setColor(Color.black);
+/*  85 */
+        //this.gPrim.drawRect(0, 0, 100, 100);
+/*     */
+    }
+
+    /*     */
 /*     */   
-/*     */   public void montrerImage() {}
-/*     */ 
+/*     */
+    public void montrerImage() {
+    }
+
+    /*     */
 /*     */   
-/*     */   public String getDossier() {
-/*  93 */     return this.dossier;
-/*     */   }
+/*     */
+    public String getDossier() {
+/*  93 */
+        return this.dossier;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public String getPrefix() {
+/*  97 */
+        return this.prefix;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public int getLargeur() {
+/* 101 */
+        return this.largeur;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public int getHauteur() {
+/* 105 */
+        return this.hauteur;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public int getFrame() {
+/* 109 */
+        return this.frame;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public BufferedImage getImage() {
+/* 113 */
+        return this.image;
+/*     */
+    }
+
+    /*     */
+/*     */
+    public static void doIt() {
+    }
+
+    /*     */
+/* 118 */
+    public BaseMovieGenerator() {
+        this.frame0 = 0;
+        pixelart.base.Directories d = new pixelart.base.Directories();
+        int i = 0;
+        d.setBaseDir("");
+        d.setMovieDir(getClass());
+        this.dossier = String.valueOf(d.getMovieDir(getClass())) + File.separator;
+        File f = new File(String.valueOf(this.dossier) + File.separator + "0");
+        while (f.exists())
+/*     */ f = new File(String.valueOf(this.dossier) + File.separator + i++);
+        this.dossier = String.valueOf(f.getAbsolutePath()) + File.separator;
+        f.mkdirs();
+/*     */
+        this.prefix = "im-";
+/*     */
+        this.largeur = 1388;
+/* 122 */
+        this.hauteur = 768;
+    }
+
+    public int getFrame0() {
+        return this.frame0;
+    }
+
+    /*     */
 /*     */   
-/*     */   public String getPrefix() {
-/*  97 */     return this.prefix;
-/*     */   }
-/*     */   
-/*     */   public int getLargeur() {
-/* 101 */     return this.largeur;
-/*     */   }
-/*     */   
-/*     */   public int getHauteur() {
-/* 105 */     return this.hauteur;
-/*     */   }
-/*     */   
-/*     */   public int getFrame() {
-/* 109 */     return this.frame;
-/*     */   }
-/*     */   
-/*     */   public BufferedImage getImage() {
-/* 113 */     return this.image;
-/*     */   }
-/*     */   
-/*     */   public static void doIt() {}
-/*     */   
-/* 118 */   public BaseMovieGenerator() { this.frame0 = 0; pixelart.base.Directories d = new pixelart.base.Directories(); int i = 0; d.setBaseDir(""); d.setMovieDir(getClass()); this.dossier = String.valueOf(d.getMovieDir(getClass())) + File.separator; File f = new File(String.valueOf(this.dossier) + File.separator + "0"); while (f.exists())
-/*     */       f = new File(String.valueOf(this.dossier) + File.separator + i++);  this.dossier = String.valueOf(f.getAbsolutePath()) + File.separator; f.mkdirs();
-/*     */     this.prefix = "im-";
-/*     */     this.largeur = 1388;
-/* 122 */     this.hauteur = 768; } public int getFrame0() { return this.frame0; }
-/*     */ 
-/*     */   
-/*     */   public void doit(Hashtable<String, Object> params) {
-/* 126 */     this.largeur = Integer.parseInt((String)params.get("largeur"));
-/* 127 */     this.hauteur = Integer.parseInt((String)params.get("hauteur"));
-/* 128 */     this.frame0 = 0;
-/* 129 */     init(params);
-/* 130 */     if (params.containsKey("ecran"))
-/* 131 */       initMontrerImage(); 
-/* 132 */     for (int i = 0; i < ((Integer)params.get("nombre")).intValue(); i++) {
-/* 133 */       creerImage();
-/* 134 */       initImage();
-/* 135 */       dessiner();
-/*     */       try {
-/* 137 */         enregistrerImage();
-/* 138 */         System.out.println(i);
-/* 139 */       } catch (IOException e) {
-/* 140 */         e.printStackTrace();
-/*     */       } 
-/* 142 */       this.frame0++;
-/* 143 */       this.label.setText(String.valueOf(this.frame0));
-/*     */     } 
-/*     */   }
-/*     */   
-/*     */   public void doit(Hashtable<String, Object> hashtable, JLabel nbrImages) {
-/* 148 */     this.label = nbrImages;
-/* 149 */     doit(hashtable);
-/*     */   }
-/*     */   
-/*     */   public abstract void init(Hashtable<String, Object> paramHashtable);
-/*     */   
-/*     */   public abstract void initImage();
-/*     */   
-/*     */   public abstract void dessiner();
-/*     */   
-/*     */   public abstract void modifierImage();
-/*     */ }
+/*     */
+    public void doit(Hashtable<String, Object> params) {
+/* 126 */
+        this.largeur = Integer.parseInt((String) params.get("largeur"));
+/* 127 */
+        this.hauteur = Integer.parseInt((String) params.get("hauteur"));
+/* 128 */
+        this.frame0 = 0;
+/* 129 */
+        init(params);
+/* 130 */
+        if (params.containsKey("ecran"))
+/* 131 */ initMontrerImage();
+/* 132 */
+        for (int i = 0; i < ((Integer) params.get("nombre")).intValue(); i++) {
+/* 133 */
+            creerImage();
+/* 134 */
+            initImage();
+/* 135 */
+            dessiner();
+/*     */
+            try {
+/* 137 */
+                enregistrerImage();
+/* 138 */
+                System.out.println(i);
+/* 139 */
+            } catch (IOException e) {
+/* 140 */
+                e.printStackTrace();
+/*     */
+            }
+/* 142 */
+            this.frame0++;
+/* 143 */
+            this.label.setText(String.valueOf(this.frame0));
+/*     */
+        }
+/*     */
+    }
+
+    /*     */
+/*     */
+    public void doit(Hashtable<String, Object> hashtable, JLabel nbrImages) {
+/* 148 */
+        this.label = nbrImages;
+/* 149 */
+        doit(hashtable);
+/*     */
+    }
+
+    /*     */
+/*     */
+    public abstract void init(Hashtable<String, Object> paramHashtable);
+
+    /*     */
+/*     */
+    public abstract void initImage();
+
+    /*     */
+/*     */
+    public abstract void dessiner();
+
+    /*     */
+/*     */
+    public abstract void modifierImage();
+/*     */
+
+
+    public void setRgb(Color color, int x, int y) {
+        g2.setColor(color);
+/* 51 */
+        g2.drawRect(x, y, 1, 1);
+        g.drawRect((int)(1.0*x/image.getWidth()*jPanel.getWidth()),
+                (int)(1.0*y/image.getHeight()*jPanel.getHeight())
+                , 1, 1);
+    }
+}
 
 
 /* Location:              C:\Users\P1779\Downloads\pixelart.jar!\pixelart\base\BaseMovieGenerator.class
--- src/main/java/pixelart/exemples/neuralnetwerken/Colors.java
+++ src/main/java/pixelart/exemples/neuralnetwerken/Colors.java
@@ -1,91 +1,143 @@
-/*    */ package pixelart.exemples.neuralnetwerken;
+/*    */
+package pixelart.exemples.neuralnetwerken;
 /*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Graphics;
+/*    */
+
+import java.awt.*;
+/*    */
 /*    */ import java.io.IOException;
 /*    */ import java.util.Hashtable;
 /*    */ import pixelart.base.BaseMovieGenerator;
-/*    */ 
+
+/*    */
 /*    */ public class Colors
-/*    */   extends BaseMovieGenerator
+/*    */ extends BaseMovieGenerator
 /*    */ {
-/* 12 */   private pixelart.exemples.neuralnetwerken.ColorNetwerk cn = null;
-/*    */   
-/*    */   public Colors(String dossier, String prefix, int largeur, int hauteur) {
-/* 15 */     super(largeur, hauteur);
-/* 16 */     this.cn = new pixelart.exemples.neuralnetwerken.ColorNetwerk(largeur, hauteur, 20, 5);
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public Colors() {
-/* 22 */     this.cn = new pixelart.exemples.neuralnetwerken.ColorNetwerk(this.largeur, this.hauteur, 20, 5);
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void initImage() {
-/* 28 */     this.cn.update();
-/* 29 */     this.cn.compute();
-/* 30 */     int x = this.largeur / 2;
-/* 31 */     int y = this.hauteur / 2;
-/* 32 */     System.out.println("Max =  " + (this.cn.getRes()[1][x][y] * this.cn.getFacteur()));
-/* 33 */     this.gPrim.setColor(Color.black);
-/*    */   }
+    private static final int NUMFRAMES = 25 * 60 * 6;
+    /* 12 */   private pixelart.exemples.neuralnetwerken.ColorNetwerk cn = null;
+
+    /*    */
+/*    */
+    public Colors(String dossier, String prefix, int largeur, int hauteur) {
+/* 15 */
+        super(largeur, hauteur);
+/* 16 */
+        this.cn = new pixelart.exemples.neuralnetwerken.ColorNetwerk(largeur, hauteur, 20, 5);
+/*    */
+    }
+
+    /*    */
 /*    */ 
 /*    */   
-/*    */   public void dessiner() {
-/*    */     int x;
-/* 39 */     for (x = 0; x < this.largeur; x++) {
-/* 40 */       for (int i = 0; i < this.hauteur; i++) {
-/* 41 */         Graphics g2 = null;
-/* 42 */         if (this.gPrim != null) {
-/* 43 */           g2 = this.gPrim;
-/*    */         } else {
-/* 45 */           g2 = this.jPanel.getGraphics();
-/*    */         } 
-/* 47 */         g2.setColor(new Color((
-/* 48 */               (int)(this.cn.getRes()[0][x][i] * this.cn.getFacteur()) << 16) + (
-/* 49 */               (int)(this.cn.getRes()[1][x][i] * this.cn.getFacteur()) << 8) + 
-/* 50 */               (int)(this.cn.getRes()[2][x][i] * this.cn.getFacteur())));
-/* 51 */         this.gPrim.drawLine(x, i, x, i);
-/*    */       } 
-/* 53 */     }  x = this.largeur / 2;
-/* 54 */     int y = this.hauteur / 2;
-/* 55 */     Color c = new Color((
-/* 56 */         (int)(this.cn.getRes()[0][x][y] * this.cn.getFacteur()) << 16) + (
-/* 57 */         (int)(this.cn.getRes()[1][x][y] * this.cn.getFacteur()) << 8) + 
-/* 58 */         (int)(this.cn.getRes()[2][x][y] * this.cn.getFacteur()));
-/* 59 */     System.out.print("ColorTools: " + x + ",y" + y + " : " + c.getRed() + ", " + 
-/* 60 */         c.getGreen() + ", " + c.getBlue());
-/*    */   }
-/*    */ 
+/*    */
+    public Colors() {
+/* 22 */
+        this.cn = new pixelart.exemples.neuralnetwerken.ColorNetwerk(this.largeur, this.hauteur, 20, 5);
+/*    */
+    }
+
+    /*    */
 /*    */ 
 /*    */   
-/*    */   public void modifierImage() {}
+/*    */
+    public void initImage() {
+/* 28 */
+        this.cn.update();
+/* 29 */
+        this.cn.compute();
+/* 30 */
+        int x = this.largeur / 2;
+/* 31 */
+        int y = this.hauteur / 2;
+/* 32 */
+        System.out.println("Max =  " + (this.cn.getRes()[1][x][y] * this.cn.getFacteur()));
+/* 33 */
+        g = (Graphics2D) image.getGraphics();
+        g.setColor(Color.black);
+/*    */
+    }
+
+    /*    */
+/*    */   
+/*    */
+    public void dessiner() {
+/*    */
+/* 39 */
+        Graphics aThisjPanelGraphics = this.jPanel.getGraphics();
+        for (int x = 0; x < this.largeur; x++) {
+/* 40 */
+            for (int y = 0; y < this.hauteur; y++) {
+/* 41 */
+                Graphics g2 = null;
+                g2 = this.image.getGraphics();
+/* 47 */
+
+                Color color =new Color((
+/* 48 */               (int) (this.cn.getRes()[0][x][y] * this.cn.getFacteur()) << 16) + (
+/* 49 */               (int) (this.cn.getRes()[1][x][y] * this.cn.getFacteur()) << 8) +
+/* 50 */               (int) (this.cn.getRes()[2][x][y] * this.cn.getFacteur())) ;
+
+                g2.setColor(color);
+/* 51 */
+                setRgb(color, x, y);
+            }
+/* 53 */
+        }
+        aThisjPanelGraphics.drawImage(image, 0, 0, jPanel.getWidth(), jPanel.getHeight(), null);
+        System.out.println("+");
+/*    */
+    }
+
+    /*    */
 /*    */ 
 /*    */   
-/*    */   public static void main(String[] args) {
-/* 69 */     String path = "outimg\\";
-/* 70 */     if (args.length > 0)
-/* 71 */       path = args[0]; 
-/* 72 */     Colors c = new Colors(path, "im2-", 1388, 768);
-/* 73 */     c.initMontrerImage();
-/* 74 */     for (int i = 0; i < 10; i++) {
-/* 75 */       c.creerImage();
-/* 76 */       c.initImage();
-/* 77 */       c.dessiner();
-/*    */       try {
-/* 79 */         c.enregistrerImage();
-/* 80 */       } catch (IOException e) {
+/*    */
+    public void modifierImage() {
+    }
+
+    /*    */
+/*    */   
+/*    */
+    public static void main(String[] args) {
+/* 69 */
+        String path = "outimg\\";
+/* 70 */
+        if (args.length > 0)
+/* 71 */ path = args[0];
+/* 72 */
+        Colors c = new Colors(path, "im2-", 1388, 768);
+/* 73 */
+        c.initMontrerImage();
+/* 74 */
+        for (int i = 0; i < NUMFRAMES; i++) {
+/* 75 */
+            c.creerImage();
+/* 76 */
+            c.initImage();
+/* 77 */
+            c.dessiner();
+/*    */
+            try {
+/* 79 */
+                c.enregistrerImage();
+/* 80 */
+            } catch (IOException e) {
 /*    */         
-/* 82 */         e.printStackTrace();
-/*    */       } 
-/*    */     } 
-/*    */   }
-/*    */   
-/*    */   public void init(Hashtable<String, Object> objects) {}
-/*    */ }
+/* 82 */
+                e.printStackTrace();
+/*    */
+            }
+/*    */
+        }
+/*    */
+    }
+
+    /*    */
+/*    */
+    public void init(Hashtable<String, Object> objects) {
+    }
+/*    */
+}
 
 
 /* Location:              C:\Users\P1779\Downloads\pixelart.jar!\pixelart\exemples\neuralnetwerken\Colors.class
