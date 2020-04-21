6454515f74472118ddc38f1a25e5643dc138ba1d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-21 20:42:36 +0200

    Update Vec8.java

--- src/main/java/pixelart/base/Vec8.java
+++ src/main/java/pixelart/base/Vec8.java
@@ -4,19 +4,23 @@
 package pixelart.base;
 import one.empty3.library.shader.Vec;
 public class Vec8 extends BaseMovieGenerator {
+     Timer timer;
      private Vec[][] vec ;
      
      public Vec8(int rx, int ry) {
           vec = new Vec[rx] [ry] ;
      } 
-     public double time() {} 
+     public double time() {
+         return timer.getTimeElapsed();
+
+     } 
      public Vec coordXY() {
          
      
      } 
      public void color(Vec c) {} 
      public void loop() {
-          Timer timer = new Timer() ;
+          timer = new Timer() ;
           for(int i= 0; i<vec.length; i++) {
                
               for(int j= 0; j<vec[i] .length; j++) {
