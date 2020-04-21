4be701194fd36c235b41cdbdbef137414f221e8d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-04-21 20:32:47 +0200

    Update Vec8.java

--- src/main/java/pixelart/base/Vec8.java
+++ src/main/java/pixelart/base/Vec8.java
@@ -3,9 +3,10 @@
 **/
 package pixelart.base;
 public class Vec8 extends BaseMovieGenerator {
+     private Vec[][] vec ;
      
-     
-     public Vec8() {
+     public Vec8(int rx, int ry) {
+          vec = new Vec[rx] [ry] ;
      } 
      public double time() {} 
      public Vec coordXY() {
@@ -13,6 +14,16 @@ public class Vec8 extends BaseMovieGenerator {
      
      } 
      public void color(Vec c) {} 
-     public void loop() {} 
-     public void main() {} 
+     public void loop() {
+          for(int i= 0; i<vec.length; i++) {
+               
+              for(int j= 0; j<vec[i] .length; j++) {
+                   coordXY = new Vec ((double)i,
+                                     (double)j ) ;
+              } 
+          } 
+     } 
+     public void main() {
+          
+     } 
 } 
